747256b8d2662b43e98f8384cfff31ed7d895bcf
Husain Boltwala <hbolty@gmail.com>
2021-08-31 23:55:21 +0530

    Branch A Class2 Changes

--- src/test/Class2.java
+++ src/test/Class2.java
@@ -3,5 +3,7 @@ package test;
 public class Class2 {
 	public static void main(String [] args) {
 		System.out.println("Test123");
+		System.out.println("Line6 A");
+		System.out.println("Line7 A");
 	}
 }
